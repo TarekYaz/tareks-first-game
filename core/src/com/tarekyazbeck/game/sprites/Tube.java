6e68279c4c1816b51a42c689f9b055f3c903386d
Tarek Yazbeck <markyazbeck94@gmail.com>
2021-02-04 18:44:29 +0000

    playable with moving tubes no collision

---
+++ core/src/com/tarekyazbeck/game/sprites/Tube.java
@@ -0,0 +1,47 @@
+package com.tarekyazbeck.game.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
+
+public class Tube {
+    public static final int TUBE_WIDTH = 52;
+
+    private static final int FLUCTUATION = 130;
+    private static final int TUBE_GAP = 100;
+    private static final int LOWEST_OPENING = 120;
+    private Texture topTube, bottomTube;
+    private Vector2 posTopTube, posBotTube;
+    private Random rand;
+
+    public Tube(float x){
+        topTube = new Texture("pipe-green-top.png");
+        bottomTube = new Texture("pipe-green-bottom.png");
+        rand = new Random();
+
+        posTopTube = new Vector2(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBotTube = new Vector2(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+    }
+
+    public Texture getTopTube() {
+        return topTube;
+    }
+
+    public Texture getBottomTube() {
+        return bottomTube;
+    }
+
+    public Vector2 getPosTopTube() {
+        return posTopTube;
+    }
+
+    public Vector2 getPosBotTube() {
+        return posBotTube;
+    }
+
+    public void reposition(float x) {
+        posTopTube.set(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBotTube.set(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+    }
+}
--- core/src/com/tarekyazbeck/game/states/PlayState.java
+++ core/src/com/tarekyazbeck/game/states/PlayState.java
@@ -3,18 +3,31 @@ package com.tarekyazbeck.game.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 import com.tarekyazbeck.game.FlappyDemo;
 import com.tarekyazbeck.game.sprites.Bird;
+import com.tarekyazbeck.game.sprites.Tube;
 
 public class PlayState extends State {
+    private static final int TUBE_SPACING = 125;
+    private static final int TUBE_COUNT = 4;
+
     private Bird bird;
     private Texture bg;
+
+    private Array<Tube> tubes;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 100);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
         bg = new Texture("background-day.png");
+
+        tubes = new Array<Tube>();
+
+        for(int i = 1; i <= TUBE_COUNT; i++){
+            tubes.add(new Tube(i * (TUBE_SPACING + Tube.TUBE_WIDTH)));
+        }
     }
 
     @Override
@@ -28,6 +41,14 @@ public class PlayState extends State {
     public void update(float dt) {
         handleInput();
         bird.update(dt);
+        cam.position.x = bird.getPosition().x + 80;
+
+        for(Tube tube : tubes){
+            if(cam.position.x - (cam.viewportWidth / 2) > tube.getPosTopTube().x + tube.getTopTube().getWidth()){
+                tube.reposition(tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));
+            }
+        }
+        cam.update();
     }
 
     @Override
@@ -36,6 +57,10 @@ public class PlayState extends State {
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        for(Tube tube : tubes) {
+            sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+            sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
+        }
         sb.end();
     }
 
